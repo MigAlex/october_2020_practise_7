8566b728086c7480e9ac04afef235b48874dd443
Alex <awangardowie@gmail.com>
2020-10-21 17:38:36 +0200

    bottom sheet

--- lib/customs/my_bottom_sheet.dart
+++ lib/customs/my_bottom_sheet.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
 
 class MyBottomSheet extends StatelessWidget {
+  final String title;
+  final VoidCallback onClickedYes;
+  final VoidCallback onClickedNo;
+
+  const MyBottomSheet(
+      {Key key,
+      @required this.title,
+      @required this.onClickedYes,
+      @required this.onClickedNo})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              title,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                RaisedButton(onPressed: onClickedNo, child: Text('No')),
+                RaisedButton(onPressed: onClickedYes, child: Text('Yes')),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/home.dart
+++ lib/home.dart
@@ -6,10 +6,13 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       body: Center(child: Text('hej')),
-      bottomSheet: MyBottomSheet(),
+      bottomSheet: MyBottomSheet(
+        title: 'Heya',
+        onClickedNo: () {},
+        onClickedYes: () {},
+      ),
     );
   }
 }
-
-
